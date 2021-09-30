3443b62ddc7d89414225971287efd3250e697da7
iamblaw <Bryant.law@lc.cuny.edu>
2021-09-29 23:32:38 -0400

    added extra exclamation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          My name is Bryant and I'm going to master React!
+          My name is Bryant and I'm going to master React!!
         </p>
         <a
           className="App-link"
